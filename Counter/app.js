f4b5fe5297dff55a83ae9d386da1812880cf4454
swara <swara.kubade@gmail.com>
2024-03-08 23:49:06 +0530

    Created a simple Counter in javascript

---
+++ Counter/app.js
@@ -0,0 +1,31 @@
+let count = 0;
+//create a reference for value
+let value = document.getElementById("value");
+
+//create a reference for buttons
+let btns = document.querySelectorAll(".btn");
+//run a forEach loop to iterate over all the elements within the btns array
+btns.forEach(function(btn) {
+ btn.addEventListener("click", function(e){
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease')){
+        count--;
+       }
+    else if(styles.contains('increase')){
+        count++;
+        }
+    else{
+        count = 0;
+       }
+       if(count > 0){
+        value.style.color = "green";
+       }
+       else if(count < 0){
+        value.style.color = "red";
+       }
+       else{
+        value.style.color = "black";
+       }
+    value.textContent = count;
+ });
+});
